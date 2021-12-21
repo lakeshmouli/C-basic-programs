c2646a9b1d9587c2d4ea77591c8d9f44fe50540d
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-21 22:23:12 +0530

    Create define-program.c

---
+++ Define-program/define-program.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#defien PI 3.14 (DON’T USE =)
+
+void main()
+
+{
+
+ int r, area ;
+
+ printf("Please enter the radius of circle : ") ;
+
+ scanf("%d", &r) ;
+
+ area = PI * (r * r) ;
+
+ printf("Area of the circle = %d", area) ;
+
+}
+
+
+
