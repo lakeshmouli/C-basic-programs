45a6be70643bae532b3262eb4aec9d4b1eb2d072
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-21 22:47:17 +0530

    Create Conditional.c

---
+++ Expressions/Conditional_Expresion/Conditional.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+ int x=1, y ;
+
+ y = ( x ==1 ? 2 : 0 ) ;
+
+ printf("x value is %d\n", x);
+
+ printf("y value is %d", y);
+
+}
+
+output:
+
+x value is 1
+
+y value is 2
