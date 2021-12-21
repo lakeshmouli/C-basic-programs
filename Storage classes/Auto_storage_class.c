81a5df9908ba1528c8e9ad31111169d11a2a59c8
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-21 22:30:50 +0530

    Create Auto_storage_class.c

---
+++ Storage classes/Auto_storage_class.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+ int main() 
+
+ { 
+
+ int a; /* default auto type */ 
+
+ auto char b; 
+
+ auto float c; 
+
+ printf("%d %c %f",a,b,c); /* printing initial default value of automatic variables a, b, and c. */ 
+
+ return 0; 
+
+ }
+
+Output
