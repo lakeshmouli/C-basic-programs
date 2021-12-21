ba644c66b2acc1cddd046dfb623569d3128daeb0
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-21 22:42:43 +0530

    Create Logical_AND_operator.c

---
+++ Expressions/Logical_operators/Logical_AND_operator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int a = 20;
+
+int b = 10;
+
+int c = 15;
+
+if(a<b && b<c)
+
+printf(" C is the biggest number of all");
+
+else
+
+printf(" C is not biggest number of all");
+
+return 0;
+}
