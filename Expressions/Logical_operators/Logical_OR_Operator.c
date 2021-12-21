afc42ba0900f23b9bacec00021845f7227c1e585
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-21 22:43:37 +0530

    Create Logical_OR_Operator.c

---
+++ Expressions/Logical_operators/Logical_OR_Operator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int a = 20;
+
+int b = 10;
+
+int c = 15;
+
+if(c>a || c>b)
+
+printf(" C is not smallest and may not biggest of all ");
+
+else
+
+printf(" C is smallest of all");
+
+return 0;
+
+}
