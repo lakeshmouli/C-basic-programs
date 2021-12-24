4595f430553233cea0a52f9d7b44ebf9265bf239
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-24 21:55:49 +0530

    Create Even_odd.c

---
+++ Simple_programs_in_c/Even_odd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+ int n ;
+
+ clrscr() ;
+
+ printf("Enter any integer number: ") ;
+
+ scanf("%d", &n) ;
+
+ if( n%2 == 0 )
+
+ printf("Given number is EVEN\n") ;
+
+ else
+
+ printf("Given number is ODD\n") ;
+
+}
