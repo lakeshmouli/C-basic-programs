ba9f57d3a21945f257b7b08f1c8971ac5f2fc6e9
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-24 21:27:06 +0530

    Create Typeconversion.c

---
+++ Type casting and type conversion/Typeconversion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+
+{
+
+int i = 95 ;
+
+float x = 90.99 ;
+
+char ch = 'A' ;
+
+i = x ;
+
+printf("i value is %d\n",i);
+
+x = i ;
+
+printf("x value is %f\n",x);
+
+i = ch ;
+
+printf("i value is %d\n",i);
+
+}
+
