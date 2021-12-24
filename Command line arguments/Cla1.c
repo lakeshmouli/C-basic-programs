73b91b49fd82470b6e47d646702838962b40b955
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-24 21:30:32 +0530

    Create Cla1.c

---
+++ Command line arguments/Cla1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main( int argc, char *argv [] )
+
+{
+
+printf(" \n First arg is %s \n", argv[0]);
+
+printf(" \n 2nd arg is %s \n", argv[1]);
+
+printf(" \n 3rd arg is %s \n", argv[2]);
+
+printf(" \n 4th arg is %s \n", argv[3]);
+
+return 0;
+
+}
+
