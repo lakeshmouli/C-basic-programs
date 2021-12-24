a2c31179c95fe45c6f54876ba96b94582913865f
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-24 21:48:05 +0530

    Create Relationaloperator.c

---
+++ Operators/Relationaloperator.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+ int main()
+
+ {
+
+ int a = 5, b = 5, c = 10;
+
+ printf("%d == %d is %d \n", a, b, a == b);
+
+ printf("%d == %d is %d \n", a, c, a == c);
+
+ printf("%d > %d is %d \n", a, b, a > b);
+ printf("%d > %d is %d \n", a, c, a > c);
+
+ printf("%d < %d is %d \n", a, b, a < b);
+
+ printf("%d < %d is %d \n", a, c, a < c);
+
+ printf("%d != %d is %d \n", a, b, a != b);
+
+ printf("%d != %d is %d \n", a, c, a != c);
+
+ printf("%d >= %d is %d \n", a, b, a >= b);
+
+ printf("%d >= %d is %d \n", a, c, a >= c);
+
+ printf("%d <= %d is %d \n", a, b, a <= b);
+
+ printf("%d <= %d is %d \n", a, c, a <= c);
+
+ return 0;
+
+ }
+
+/*Output
+
+5 == 5 is 1
+
+5 == 10 is 0
+
+5 > 5 is 0
+
+5 > 10 is 0
+
+5 < 5 is 0
+
+5 < 10 is 1
+
+5 != 5 is 0
+
+5 != 10 is 1
+
+5 >= 5 is 1
+
+5 >= 10 is 0
+
+5 <= 5 is 1
+
+5 <= 10 is 1*/
