a7779e22dedf5c801c15011be88f3c25b2fa1761
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-26 21:30:37 +0530

    Create Gets_method.c

---
+++ Arrays/Input_output/Gets_method.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+ char name[50];
+
+ printf("Please enter your name : ");
+
+ gets(name);
+
+ printf("The input name is %s” ,name);
+
+ return 0;
+
+}
+
