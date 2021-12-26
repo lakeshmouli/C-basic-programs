0c9f30873a241a712e6ca276811351caed209497
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-26 21:25:27 +0530

    Create String_char_Array.c

---
+++ Arrays/Strings_in_Array/String_char_Array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ char str1[6] = "Hello";
+
+ char str2[] = "Hello!";
+
+ char name1[] = {'p','p','s','w','c'};
+
+ char name2[6] = {'p','p','s','w','c'};
+
+ //char *title[20];
+
+ //title ="PPS class";
+
+ printf("%s \n",str1); 
+
+ printf("%s \n",str2);
+
+ printf("%s \n ",name1);
+
+ printf("%s \n",name2);
+
+ //printf("%s",title);
+
+ return 0;
+
+}
