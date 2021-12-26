a6db7f00986c0f95a57f127dcbf59dce7933279d
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-26 21:27:35 +0530

    Create Input_output.c

---
+++ Arrays/Input_output/Input_output.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+ char name[30];
+
+ printf(“Enter name for input: “);
+
+ gets(name); //Function to read string from user.
+
+ printf(“output Name is: “);
+
+ puts(name); //Function to display string.
+
+ return 0;
+
+}
+
