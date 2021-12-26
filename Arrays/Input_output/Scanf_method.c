dd186406ae4fd66ebf66a6ee5423adc88561b3f9
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-26 21:29:30 +0530

    Create Scanf_method.c

---
+++ Arrays/Input_output/Scanf_method.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+ char name[50];
+
+ printf("Please enter your name : ");
+
+ scanf("%s", name);
+
+ printf("The input name is %s” ,name);
+
+ return 0;
+
+}
