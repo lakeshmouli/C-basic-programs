febc71d6c662957412585bb02e3afa647bf08806
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-27 15:07:34 +0530

    Create Char_type_data_type_using_typedef.c

---
+++ Arrays/String_handling_functions/Char_type_data_type_using_typedef.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ typedef unsigned char uchar;
+
+ uchar ch = 'a';
+
+ printf("new uchar type ch is : %c \n", ch);
+
+ unsigned char ch1 = 'z';
+
+ printf("normal ch is : %c\n", ch1);
+
+ return 0;
+
+}
+
