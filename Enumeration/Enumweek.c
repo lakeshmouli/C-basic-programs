ef3958d65c3a7525a364922525bfdeff772785e5
lakeshmouli <84567381+lakeshmouli@users.noreply.github.com>
2021-12-31 11:36:48 +0530

    Create Enumweek.c

---
+++ Enumeration/Enumweek.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+//Enumeration declartion
+
+enum week{ Sunday, Monday, Tuesday, wednesday, Thursday,
+
+Friday, Saturday } ;
+
+printf("\nSunday = %d ", Sunday);
+
+printf("\nWednesday = %d ", wednesday);
+
+printf("\nSaturday = %d ", Saturday);
+
+return 0;
+
+}
+
